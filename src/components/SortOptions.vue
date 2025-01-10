7909e9aaf59cb2efae85682b30eb53dbe9ea6b3e
nsivaev <nsivaev77@gmail.com>
2025-01-10 15:39:24 +0300

    * added buttons for sorting by date and likes

--- src/components/SortOptions.vue
+++ src/components/SortOptions.vue
@@ -1,3 +1,28 @@
+<script setup lang="ts">
+import { defineEmits } from 'vue';
+
+// событие для выбора сортировки
+const emit = defineEmits<{
+  (e: "sort", playload: "date" | "likes"): void;
+}>();
+
+// функция выбора сортировки
+const onSortChange = (sortBy: "date" | "likes") => {
+  emit("sort", sortBy);
+};
+</script>
+
 <template>
-  <p>sort options</p>
+  <div class="sort-options">
+    <button @click="onSortChange('date')">Сортировать по дате</button>
+    <button @click="onSortChange('likes')">Сортировать по лайкам</button>
+  </div>
 </template>
+
+
+<style scoped>
+.sort-options {
+  display: flex;
+  gap: 10px;
+}
+</style>
