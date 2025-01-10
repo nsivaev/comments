768879a37fc27fca6cf562bc52dde18a874768e6
nsivaev <nsivaev77@gmail.com>
2025-01-10 15:06:36 +0300

    * adding comments in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,40 @@
 <script setup lang="ts">
+  import { ref } from "vue";
   import CommentForm from "@/components/CommentForm.vue";
   import SortOptions from "@/components/SortOptions.vue";
   import CommentList from "@/components/CommentList.vue";
+
+  // тип комментария
+  interface Comment {
+    id: number;
+    name: string;
+    comment: string;
+    date: Date;
+    likes: number;
+  }
+
+  // массив комментарие
+  const comments = ref<Comment[]>([]);
+
+  // добавление нового комментария
+  const addComment = (playload: { name: string; comment: string }) => {
+    const newComment: Comment = {
+      id: Date.now(),
+      name: playload.name,
+      comment: playload.comment,
+      date: new Date(),
+      likes: 0
+    }
+
+    comments.value.push(newComment);
+  }
 </script>
 
 <template>
   <main>
-    <CommentForm/>
+    <CommentForm @add-comment="addComment" />
     <SortOptions/>
-    <CommentList/>
+    <CommentList :comments="comments" />
   </main>
 </template>
 
--- src/components/CommentList.vue
+++ src/components/CommentList.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+  import { defineProps } from "vue";
   import CommentItem from "@/components/CommentItem.vue";
+
+  // получение списка комментариев через props
+  const props = defineProps<{
+    comments: {
+      id: number;
+      name: string;
+      comment: string;
+      date: Date;
+      likes: number;
+    }[];
+  }>();
 </script>
 
 <template>
