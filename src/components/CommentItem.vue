89876eb16c8a1aa2f12aa18962730335a84e26f8
nsivaev <nsivaev77@gmail.com>
2025-01-10 15:27:01 +0300

    * added display of comment data in CommentItem

--- src/components/CommentItem.vue
+++ src/components/CommentItem.vue
@@ -1,3 +1,31 @@
+<script setup lang="ts">
+import {defineProps} from "vue";
+
+// получение комментария через props
+const props = defineProps<{
+  comment: {
+    id: number;
+    name: string;
+    comment: string;
+    date: Date;
+    likes: number;
+  }
+}>();
+</script>
+
 <template>
-  <p>comment item</p>
+  <div class="comment-item">
+    <p>{{ props.comment.name }} ({{ props.comment.date.toLocaleString() }})</p>
+    <p>{{ props.comment.comment }}</p>
+    <p>{{ props.comment.likes }} 👍</p>
+  </div>
 </template>
+
+<style scoped>
+.comment-item {
+  border: 1px solid #cccccc;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+}
+</style>
